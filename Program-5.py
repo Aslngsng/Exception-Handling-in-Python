2b7416ecea8776937f5caf6e34da019fa234598e
Aslngsng <salangsang.abril@gmail.com>
2024-05-04 20:32:03 +0800

    ussed try except to handle the exception in the code

--- Program-5.py
+++ Program-5.py
@@ -10,27 +10,33 @@ while True:
 
     chosen_operation = input("What operation would you like to choose? ")
     chosen_operation = chosen_operation.upper()
+    
+    try:
+        if chosen_operation.upper() not in ["ADDITION, SUBSTRACTION, MULTIPLICATION, OR DIVISION"]:
+            raise TypeError ("Operation is not available!")
 
-    if chosen_operation == "ADDTION":
-        first_number = int(("Enter first number: "))
-        second_number = int(("Enter second number: "))
-        print("The sum is " + str(first_number + second_number))
-    elif chosen_operation == "SUBSTRACTION":
-        first_number = int(("Enter first number: "))
-        second_number = int(("Enter second number: "))
-        print("The difference is " + str(first_number - second_number))
-    elif chosen_operation == "MULTIPLICATION":
-        first_number = int(("Enter first number: "))
-        second_number = int(("Enter second number: "))
-        print("The product is " + str(first_number * second_number))
-    elif chosen_operation == "DIVISION":
-        first_number = int(("Enter first number: "))
-        second_number = int(("Enter second number: "))
-        print("The quotient is " + str(first_number // second_number))
-    else:
-        print("Invalid operation to use")
-
-    ask_the_user_if_they_want_to_try_again = print("Do you want to try again? (YES/NO): ")
-    if ask_the_user_if_they_want_to_try_again.upper() != "YES":
-        print ("Thank you")
-    break
+        if chosen_operation == "ADDITION":
+            first_number = int(("Enter first number: "))
+            second_number = int(("Enter second number: "))
+            result = print("The sum is " + str(first_number + second_number))
+        elif chosen_operation == "SUBSTRACTION":
+            first_number = int(("Enter first number: "))
+            second_number = int(("Enter second number: "))
+            result = print("The difference is " + str(first_number - second_number))
+        elif chosen_operation == "MULTIPLICATION":
+            first_number = int(("Enter first number: "))
+            second_number = int(("Enter second number: "))
+            result = print("The product is " + str(first_number * second_number))
+        elif chosen_operation == "DIVISION":
+            first_number = int(("Enter first number: "))
+            second_number = int(("Enter second number: "))
+            if second_number == 0:
+                raise ValueError ("Sorry! You are dividing by zero")
+            result = print("The quotient is " + str(first_number // second_number))
+    except:
+        print("Unkown exeption") 
+        ask_the_user_if_they_want_to_try_again = print("Do you want to try again? (YES/NO): ")
+        ask_the_user_if_they_want_to_try_again = ask_the_user_if_they_want_to_try_again.upper()
+        if ask_the_user_if_they_want_to_try_again.upper() != "YES":
+            print ("Thank you")
+        break
